d49d21b2f2a19617103523017a10b287278658c7
DavidLC578 <davidlatt2002@gmail.com>
2025-02-10 16:54:13 -0500

    idk

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
+        $user =
+            $posts = Post::orderBy('created_at', 'desc')->paginate(5);
         return view('posts.index', compact('posts'));
     }
 
